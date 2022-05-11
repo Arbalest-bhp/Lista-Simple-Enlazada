66144e513de3a091ebabce2b6d3414562aa9f429
Arbalest-bhp <100734513+Arbalest-bhp@users.noreply.github.com>
2022-05-10 20:31:12 -0500

    Update ProgramaListas.java

--- ProgramaListas.java
+++ ProgramaListas.java
@@ -5,6 +5,11 @@ public class ProgramaListas {
 		ListaSimple miLista = new ListaSimple();
 
 		miLista.insertaPrimerNodo("A");
+		miLista.imprimir();
+		miLista.insertarAntesPrimerNodo("B");
+		miLista.imprimir();
+		miLista.insertarAntesPrimerNodo("C");
+		miLista.imprimir();
 
 	}
 }
